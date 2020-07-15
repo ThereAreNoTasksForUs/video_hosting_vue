01edc3706d84a209b016beae147e56d286f9e72b
Azazel.h <arseniy2003bulgakov@gmail.com>
2020-07-15 16:42:38 +0300

    Base for register page

---
+++ src/http-common.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export const HTTP = axios.create({
+    baseURL: 'http://localhost:8080/api/',
+    headers: {
+        'Content-type': 'application/json',
+        'Authorization': 'Bearer {token}',
+    }
+})
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,12 +1,60 @@
 <template>
     <div>
-        RegisterPage
+        <form>
+            <label>
+                Email:
+                <input type="email" v-model="email">
+            </label>
+            <label>
+                Username:
+                <input v-model="username">
+            </label>
+            <label>
+                Password:
+                <input type="password" v-model="password">
+            </label>
+            <button type="button" @click="post_request">Submit</button>
+        </form>
+
+        <p v-if="error">
+            {{error.message}}
+        </p>
+
+        {{ this.username }}
+        {{ this.email }}
+        {{ this.response }}
     </div>
 </template>
 
 <script>
+    import {HTTP} from '../http-common.js'
+
     export default {
-        name: "Register"
+        data() {
+            return {
+                response: [],
+                email: '',
+                username: '',
+                password: '',
+                error: null,
+            }
+        },
+        methods: {
+            post_request: function(){
+                HTTP.post(`auth/register`, {
+                    email: this.email,
+                    username: this.username,
+                    password: this.password,
+                })
+                    .then(response => {
+                        this.response = response.data
+                        console.log(this.response)
+                    })
+                    .catch(e => {
+                        this.error = e;
+                    })
+            }
+        }
     }
 </script>
 
